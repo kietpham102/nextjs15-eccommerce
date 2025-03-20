89007fc6cc3590d707b82fb385efc933a72c2fd2
Kiet Pham <anhkietpham.hcmus@gmail.com>
2025-03-20 22:10:36 +0700

    Best selling slider products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,7 +2,7 @@
 'use server'
 
 import { connectToDatabase } from '@/lib/db'
-import Product from '@/lib/db/models/product.model'
+import Product, {IProduct} from '@/lib/db/models/product.model'
 
 export async function getAllCategories() {
   await connectToDatabase()
@@ -34,4 +34,21 @@ export async function getProductsForCard({
     href: string
     image: string
   }[]
+}
+// GET PRODUCTS BY TAG
+export async function getProductsByTag({
+  tag,
+  limit = 10,
+}: {
+  tag: string
+  limit?: number
+}) {
+  await connectToDatabase()
+  const products = await Product.find({
+    tags: { $in: [tag] },
+    isPublished: true,
+  })
+    .sort({ createdAt: 'desc' })
+    .limit(limit)
+  return JSON.parse(JSON.stringify(products)) as IProduct[]
 }
