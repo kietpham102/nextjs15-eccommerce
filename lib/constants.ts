44e7f9d575be88ed6008b60c010afa215cd69cea
Kiet Pham <anhkietpham.hcmus@gmail.com>
2025-03-21 13:50:22 +0700

    product detail page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,8 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ecommerce Store'
-   export const APP_SLOGAN =
-     process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
-   export const APP_DESCRIPTION =
-     process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-     'Ecommerce Store built with Next.js and MongoDB'
+export const APP_SLOGAN =
+process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION =
+process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+'Ecommerce Store built with Next.js and MongoDB'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
