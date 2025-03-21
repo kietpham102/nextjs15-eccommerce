46761ba7ccce8916c3c086c4fcbe0cbb978054fb
Kiet Pham <anhkietpham.hcmus@gmail.com>
2025-03-21 17:02:45 +0700

    add to cart

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,3 +6,6 @@ process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
 'Ecommerce Store built with Next.js and MongoDB'
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
+export const FREE_SHIPPING_MIN_PRICE = Number(
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+)
